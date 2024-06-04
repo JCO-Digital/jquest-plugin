6e8ab9162e6912ea1f9a7497f8256277ae167c87
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-06-04 11:26:01 +0300

    chore(release): version 0.3.0 🔖

--- jcore-broiler.php
+++ jcore-broiler.php
@@ -7,7 +7,7 @@
  * Author URI:      https://jco.fi
  * Text Domain:     jcore-broiler
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.3.0
  *
  * @package         JcoreBroiler
  */
