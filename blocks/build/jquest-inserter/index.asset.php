913be11eb4c5ca3fc9c67d3fd914ae4d7c4231a9
Linus <liinus.wiberg@gmail.com>
2024-08-08 14:38:42 +0300

    Fix: ??? test (might be bad code)

--- blocks/build/jquest-inserter/index.asset.php
+++ blocks/build/jquest-inserter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8f30047af369ee8320c0');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e8bea4749e4a947630de');
