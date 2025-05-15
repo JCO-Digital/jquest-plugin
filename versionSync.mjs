2cda3673bfea38f9650618411c481f21db8ef633
Maxemilian Grönblom <maxemiliang@gmail.com>
2025-05-15 17:10:31 +0300

    feat: Add versionSync script to update plugin version in files ✨

---
+++ versionSync.mjs
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from "fs";
+
+try {
+  const pluginFileName = "jquest-plugin.php";
+  const pack = JSON.parse(readFileSync("package.json"));
+  const baseFile = readFileSync(pluginFileName);
+  const baseString = baseFile
+    .toString()
+    .replace(/^(.*)Version:.*$/m, `$1Version: ${pack.version}`);
+  writeFileSync(pluginFileName, baseString);
+
+  const readmeFile = readFileSync("README.txt");
+  const readmeString = readmeFile
+    .toString()
+    .replace(/^(.*)Stable tag:.*$/m, `$1Stable tag: ${pack.version}`);
+  writeFileSync("README.txt", readmeString);
+} catch (error) {
+  console.error(error);
+}
