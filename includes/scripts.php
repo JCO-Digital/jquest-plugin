304297875dc891880220c1998c03ee1dea67695a
Maxemilian Grönblom <maxemiliang@gmail.com>
2025-09-22 12:08:35 +0300

    fix: dont crash on maybe_insert_jquest_scripts

--- includes/scripts.php
+++ includes/scripts.php
@@ -21,43 +21,46 @@ function maybe_insert_jquest_script() {
 	$has_jquest_blocks = false;
 	$needs_staging     = false;
 
-	$blocks = parse_blocks( get_the_content() );
+	$post = get_post();
+	if ( ! $post ) {
+		return;
+	}
+
+	$blocks = parse_blocks( $post->post_content );
 	foreach ( $blocks as $block ) {
 
-		$results = contains_jquest_insterter($block);
+		$results = contains_jquest_insterter( $block );
 
 		$has_jquest_blocks = $results['has_jquest_blocks'];
-		$needs_staging = $results['needs_staging'];
+		$needs_staging     = $results['needs_staging'];
 
 		// Can only add one script at a time. So might as well break.
 		if ( $needs_staging ) {
 			break;
 		}
-
 	}
 	if ( $has_jquest_blocks ) {
 		$url = $needs_staging
 		? 'https://files.jquest.fi/jquest/staging/jquest-staging.js'
 		: 'https://files.jquest.fi/jquest/jquest.js';
 
-		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+     // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 		?>
 		<script
 				data-cookieconsent-ignore='true'
 				type='module'
 				src='<?php echo $url; ?>'></script>
 		<?php
-		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
+     // phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 
-function contains_jquest_insterter($block): array
-{
+function contains_jquest_insterter( $block ): array {
 
-	$return_value = [
+	$return_value = array(
 		'has_jquest_blocks' => false,
-		'needs_staging' 	=> false
-	];
+		'needs_staging'     => false,
+	);
 
 	if ( 'jquest-inserter/jquest-inserter' === $block['blockName'] ) {
 		$return_value['has_jquest_blocks'] = true;
@@ -71,17 +74,16 @@ function contains_jquest_insterter($block): array
 		}
 	}
 
-	if( array_key_exists( 'innerBlocks', $block ) ) {
+	if ( array_key_exists( 'innerBlocks', $block ) ) {
 		foreach ( $block['innerBlocks'] as $inner_block ) {
-			$inner_values = contains_jquest_insterter($inner_block);
+			$inner_values = contains_jquest_insterter( $inner_block );
 
 			$return_value['has_jquest_blocks'] = $return_value['has_jquest_blocks'] || $inner_values['has_jquest_blocks'];
-			$return_value['needs_staging'] = $return_value['needs_staging'] || $inner_values['needs_staging'];
+			$return_value['needs_staging']     = $return_value['needs_staging'] || $inner_values['needs_staging'];
 		}
 	}
 
 	return $return_value;
-
 }
 
 add_action( 'wp_head', __NAMESPACE__ . '\maybe_insert_jquest_script' );
